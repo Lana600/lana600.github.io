6f43b601e25f48e96d3e8beb5d41c755e5080525
Lana600 <30658899+Lana600@users.noreply.github.com>
2018-04-05 19:28:48 +0400

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+	$(".toggle-menu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-menu").slideToggle();
+	});
+
+	$(".section-content .info-item").equalHeights();
+	$(".s1-bottom .info-item").equalHeights();
+
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	$(".homesect .section-bottom .buttons").click(function() {
+		$("#callback h4").html($(this).text());
+		$("#callback input[name=formname]").val($(this).text());
+	}).magnificPopup({
+		type:"inline",
+		mainClass: 'mfp-forms'
+	});
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
